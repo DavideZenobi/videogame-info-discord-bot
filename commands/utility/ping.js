b880c35ae350f015ad4846d1c3a925bf4eeef29a
gandar <davidezenobigarcia@gmail.com>
2024-12-31 23:12:34 +0100

    commands path code

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -2,7 +2,7 @@ const { SlashCommandBuilder } = require("discord.js");
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName("/ping")
+        .setName("ping")
         .setDescription("Replies with pong!"),
     async execute(interaction) {
         await interaction.reply("Pong!");
