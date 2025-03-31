ef1faa4472c0073424732ea19bee49c12f452e05
DavideZenobi <davidezenobigarcia@gmail.com>
2025-03-31 18:43:12 +0200

    api code

--- rawg-api.js
+++ rawg-api.js
@@ -3,6 +3,23 @@ require('dotenv').config();
 const API_URL = 'https://api.rawg.io/api/games/';
 const RAWG_API_TOKEN = process.env.RAWG_API_TOKEN;
 
+console.log(getGamesByString("world of warcraft"));
+
+function getGamesByString(input) {
+    const params = new URLSearchParams({
+        key: API_URL,
+        search: input,
+        page: 1,
+        page_size: 10,
+    });
+
+    return fetch(API_URL + params)
+        .then(response => response.json())
+        .then(data => data.results)
+        .catch(error => console.error(error));
+}
+
+
 function queryGame(gameName) {
     return fetch(API_URL + `${gameName}` + `?key=${RAWG_API_TOKEN}`)
         .then(response => response.json())
